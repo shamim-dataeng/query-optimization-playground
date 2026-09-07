2ca73276c01736f1a6adb96e3eb9f82feb910385
shamim-dataeng <datapulse2804@gmail.com>
2026-09-07 22:54:45 +0530

    chore: isolate it to loc-107

--- scripts/main.sql
+++ scripts/main.sql
@@ -8,7 +8,7 @@ FROM
 GROUP BY
     PULocationID
 ORDER BY 
-    PULocationID;
+    Number_Of_Trips DESC;
 
 -- Question : For each pickup zone (PULocationID), find the 3 trips with the highest fare_amount that started there. Not the top 3 fares overall — top 3 within each zone, so a busy zone and a quiet zone each contribute their own top 3.
 
@@ -20,10 +20,9 @@ SELECT
 FROM 
     my_taxi
 WHERE 
-    PULocationID=251;
+    PULocationID=107;
 
 -- M1 : Window Function
-
 WITH CTE1 AS (
     SELECT
         PULocationID,
@@ -38,10 +37,9 @@ SELECT
 FROM
     CTE1
 WHERE
-    rk<=3;
+    rk<=3 and PULocationID=107;
 
 -- M2: Correlated Subquery
-
 SELECT
     t.PULocationID,
     t.fare_amount
@@ -51,10 +49,9 @@ WHERE (
     FROM my_taxi t2
     WHERE t2.PULocationID = t.PULocationID
       AND t2.fare_amount > t.fare_amount
-) < 3;
+) < 3 and PULocationID=107;
 
--- M3: Self join 
-
+-- M3: Self join  
 SELECT
     t1.PULocationID,
     t1.fare_amount
@@ -62,6 +59,7 @@ FROM my_taxi t1
 JOIN my_taxi t2
     ON t2.PULocationID = t1.PULocationID
    AND t2.fare_amount > t1.fare_amount
+where t1.PULocationID=107
 GROUP BY
     t1.PULocationID,
     t1.fare_amount
